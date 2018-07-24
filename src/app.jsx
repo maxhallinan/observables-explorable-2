7e71a357c07c4a784d964fd7b218e8217b051f47
Max Hallinan <maxhallinan@riseup.net>
2018-07-24 07:04:04 +0200

    Set default state of range input

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,40 @@
 import xs from 'xstream'
 
+const toView = (state) => {
+  return (
+    <div>
+      <svg></svg>
+      <input 
+        className=".oe2-range-input"
+        max="1"
+        min="0" 
+        step="0.01"
+        type="range" 
+        value={state.currentRange}
+      />
+    </div>
+  );
+}
+
+const toState = ([ currentRange, ]) => ({
+  currentRange,
+});
+
 export function App (sources) {
-  const vtree$ = xs.of(
-    <div>My Awesome Cycle.js app</div>
-  )
+  const currentRange$ = sources.DOM.select('.oe2-range-input')
+    .events('input')
+    .map((event) => Number(event.target.value))
+    .startWith(1);
+
+  const stateSource$ = xs.combine(currentRange$);
+
+  const state$ = stateSource$.map(toState);
+
+  const vtree$ = state$.map(toView);
+
   const sinks = {
     DOM: vtree$
   }
+
   return sinks
 }
